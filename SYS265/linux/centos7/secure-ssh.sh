0d6481e03e54f8394e5ace38c1672f222a608174
gavin <gavin.francisco@mymail.champlain.edu>
2024-03-03 11:47:30 -0500

    secure-ssh.sh file

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,8 @@
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh/
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
+echo "Created user: $1"
